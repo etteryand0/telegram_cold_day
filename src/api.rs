b4b3da82d2a074f3634a0f4ccff3e45026187d1f
etteryand0 <etteryand@gmail.com>
2021-01-17 16:38:22 +0900

    :construction: Доп. функция дебага для Api

--- src/api.rs
+++ src/api.rs
@@ -3,8 +3,18 @@ pub struct Api {
     admin_id: String,
 }
 
+pub trait BotDebug {
+    fn traceback(&self, e: String);
+}
+
 impl Api {
     pub fn new(token: String, admin_id: String) -> Api {
         Api { token: token, admin_id: admin_id }
+    }
+}
+
+impl BotDebug for Api {
+    fn traceback(&self, e: String) {
+        
     }
 }
