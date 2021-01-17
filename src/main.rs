937621405cae26772ccf39c4b0bd8abeb9f49a07
etteryand0 <etteryand@gmail.com>
2021-01-17 15:48:13 +0900

    :construction: Простой 10 сек цикл

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
+
 fn main() {
-    println!("Hello, world!");
+    while true {
+        let now = SystemTime::now();
+
+        sleep(Duration::new(10, 0));
+        match now.elapsed() {
+            Ok(elapsed) => {
+                println!("Elapsed {} seconds", elapsed.as_secs());
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+                break;
+            }
+        }
+    }
+    println!("Script stopped");
 }
